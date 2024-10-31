510016ec85fa78f7f43767a883970b2bbba449a3
Ness <82103687+zLuciel@users.noreply.github.com>
2024-10-31 16:49:26 -0500

    add link

--- src/components/login/HoverInformation.jsx
+++ src/components/login/HoverInformation.jsx
@@ -13,7 +13,7 @@ function HoverInformation() {
           <Text size="sm">
             ¡Importante! Al iniciar sesión en nuestra plataforma, podrás acceder
             a tu cuenta si ya estás registrado en {" "}
-            <b><a href="plataformavirtual.munisjl.gob.pe" target="_blank">plataformavirtual.munisjl.gob.pe</a> </b>. Del mismo modo, si te registras en
+            <b><a href="https://plataformavirtual.munisjl.gob.pe" target="_blank">plataformavirtual.munisjl.gob.pe</a> </b>. Del mismo modo, si te registras en
             nuestra página, podrás ingresar a la plataforma del municipio. Esta
             integración te brinda mayor comodidad y flexibilidad en el acceso a
             nuestros servicios. ¡No te lo pierdas!
