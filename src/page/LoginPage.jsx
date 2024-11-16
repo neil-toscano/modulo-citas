f33cd84e22313df6efe020295e5771601c20a0b0
Ness <82103687+zLuciel@users.noreply.github.com>
2024-11-16 08:09:21 -0500

    add

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,10 +1,15 @@
 import Login from "@/components/login/Login";
-import fondo from "@/assets/fondojesus.webp";
+import fondo from "@/assets/wp_fondo.webp";
+import logo from "@/assets/logo.png";
 const LoginPage = () => {
   return (
     // login-page
-    <main className="">
-      <img className="fondo-xd" src={fondo} alt="" />
+    <main className="relative">
+      <div className="fondo-login-wp">
+        <img className="logo-wp-login" src={logo} alt="" />
+        <div className="fondo-bg-login"></div>
+      </div>
+
       <div className=" h-screen container mx-auto px-4 flex items-center justify-center">
         <Login />
       </div>
